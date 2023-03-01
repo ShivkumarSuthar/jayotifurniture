47072b666abaace4fb0c90f11c0a158f2ba10315
shivkumar <shiv.str21@gmail.com>
2023-03-01 19:47:57 +0530

    Under navigation bar done for desktop

--- src/assets/UnderNav.jsx
+++ src/assets/UnderNav.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import telephone from 'https://embed.lottiefiles.com/animation/27788';
+
 
 function UnderNav() {
   return (
@@ -9,34 +9,33 @@ function UnderNav() {
     <div className="row">
       <div className="col-lg-4 text-center mb-3 mb-lg-0">
         <div className="d-inline-flex text-left ">
-        <h1><i className="fa-solid fa-location-crosshairs font-weight-normal m-0 mx-4"></i></h1>
+        <h1><i className="fa-solid fa-location-crosshairs font-weight-normal m-0 mx-4 text-warning"></i></h1>
                    <div className="d-flex flex-column">
             <h5 style={{textAlign:"start"}}>Our Office</h5>
-           <p className="address m-0"><b>MORBI, GUJRAT 363641, INDIA</b></p>
+           <a href='/' className='text-decoration-none'><p className="address m-0 text-success"><b>MORBI, GUJRAT 363641, INDIA</b></p></a>
           </div>
         </div>
       </div>
       <div className="col-lg-4 text-center text-lg-center mb-3 mb-lg-0">
         <div className="d-inline-flex text-left">
-        <h1><i className="fa-solid fa-envelope-open-text mx-4" ></i></h1>
+        <h1><i className="fa-solid fa-envelope-open-text mx-4 text-warning" ></i></h1>
          
           <div className="d-flex flex-column">
             <h5 className='font-weight-bolder' style={{textAlign:"start"}}>Email Us</h5>
-            <a className="mail text-decoration-none" href="mailto:sanga21suthar@gmail.com"><p className="m-0 text-decoration-none">sanga21suthar@gmail.com</p></a>
+            <a className="mail text-decoration-none" href="mailto:sanga21suthar@gmail.com"><p className="m-0 text-decoration-none text-success"><b>sanga21suthar@gmail.com</b></p></a>
           </div>
         </div>
       </div>
       <div className="col-lg-4 text-center text-lg-center mb-3 mb-lg-0">
         <div className="d-inline-flex text-left">
         <h1>
-        {/* <i className="fa-duotone fa-mobile-retro m-0 mx-4"></i><i className="fa-solid fa-mobile-retro m-0 mx-4"></i> */}
-        <lottie-player src="https://assets9.lottiefiles.com/packages/lf20_PBgNop.json"  background="transparent"  speed='1' style={{width: "300px", height: "300px"}}  loop controls autoplay></lottie-player>
+        <i className="fa-solid fa-mobile-retro m-0 mx-4 text-warning"></i>
         </h1>
           
           <div className="d-flex flex-column">
             <h5 style={{textAlign:"start"}}>Call Us</h5>
             <p className="m-0">
-              <a className="tel text-decoration-none" href="tel:+919725425536"> +919725425536</a>
+              <a className="tel text-decoration-none text-success " href="tel:+919725425536"><b>+919725425536</b></a>
             </p>
           </div>
         </div>
