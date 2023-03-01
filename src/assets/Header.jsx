a20539d328742eb33f2ff3b4e1f1de66f86b576c
shivkumar <shiv.str21@gmail.com>
2023-03-01 17:00:33 +0530

    Header is done

--- src/assets/Header.jsx
+++ src/assets/Header.jsx
@@ -7,23 +7,24 @@ function Header() {
 
 
   return (
-    <div className="container-fluid position-relative nav-bar p-0">
-    <div className="container position-relative" style={{zIndex: "9"}}>
-      <nav className="navbar navbar-expand-lg bg-secondary navbar-dark py-3 py-lg-0 pl-3 pl-lg-5">
-        <a href="/" className="navbar-brand">
+    <div className="container-fluid position-relative  p-0">
+    <div className="container position-relative" style={{zIndex:9}}>
+      <nav className="navbar navbar-expand-lg bg-secondary navbar-dark py-2 py-lg-2 pl-3 pl-lg-">
+        <a href="/" className="navbar-brand ms-3">
           
           <img src={logo} alt="logo" style={{width: "60px"}} />
         </a>
-        <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+        
         <div className="collapse navbar-collapse justify-content-between px-3" id="navbarCollapse">
-          <div className="navbar-nav ml-auto py-0">
-            <a href="index.html" className="nav-item nav-link active">HOME</a>
-            <a href="about.html" className="nav-item nav-link">ABOUT US</a>
-            <a href="service.html" className="nav-item nav-link">SERVICES</a>
-            <a href="project.html" className="nav-item nav-link">OUR WORKS</a>
-            <a href="contact.html" className="nav-item nav-link">CONTACT US</a>
+          <div className="navbar-nav ms-auto ml-auto py-0">
+            <a href="index.html" className="nav-item nav-link active px-2">HOME</a>
+            <a href="about.html" className="nav-item nav-link px-2">ABOUT US</a>
+            <a href="service.html" className="nav-item nav-link px-2">SERVICES</a>
+            <a href="project.html" className="nav-item nav-link px-2">OUR WORKS</a>
+            <a href="contact.html" className="nav-item nav-link px-2">CONTACT US</a>
           </div>
         </div>
       </nav>
--- src/assets/UnderNav.jsx
+++ src/assets/UnderNav.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import telephone from 'https://embed.lottiefiles.com/animation/27788';
 
 function UnderNav() {
   return (
   
-  <div className="container-fluid bg-white py-3">
+  <div className="container bg-white py-3">
   <div className="container">
     <div className="row">
-      <div className="col-lg-4 text-left mb-3 mb-lg-0">
+      <div className="col-lg-4 text-center mb-3 mb-lg-0">
         <div className="d-inline-flex text-left ">
         <h1><i className="fa-solid fa-location-crosshairs font-weight-normal m-0 mx-4"></i></h1>
                    <div className="d-flex flex-column">
@@ -15,7 +16,7 @@ function UnderNav() {
           </div>
         </div>
       </div>
-      <div className="col-lg-4 text-left text-lg-center mb-3 mb-lg-0">
+      <div className="col-lg-4 text-center text-lg-center mb-3 mb-lg-0">
         <div className="d-inline-flex text-left">
         <h1><i className="fa-solid fa-envelope-open-text mx-4" ></i></h1>
          
@@ -25,12 +26,15 @@ function UnderNav() {
           </div>
         </div>
       </div>
-      <div className="col-lg-4 text-left text-lg-right mb-3 mb-lg-0">
+      <div className="col-lg-4 text-center text-lg-center mb-3 mb-lg-0">
         <div className="d-inline-flex text-left">
-        <h1><i className="fa-solid fa-mobile-retro m-0 mx-4"></i></h1>
+        <h1>
+        {/* <i className="fa-duotone fa-mobile-retro m-0 mx-4"></i><i className="fa-solid fa-mobile-retro m-0 mx-4"></i> */}
+        <lottie-player src="https://assets9.lottiefiles.com/packages/lf20_PBgNop.json"  background="transparent"  speed='1' style={{width: "300px", height: "300px"}}  loop controls autoplay></lottie-player>
+        </h1>
           
           <div className="d-flex flex-column">
-            <h5>Call Us</h5>
+            <h5 style={{textAlign:"start"}}>Call Us</h5>
             <p className="m-0">
               <a className="tel text-decoration-none" href="tel:+919725425536"> +919725425536</a>
             </p>
